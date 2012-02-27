d940946483c0d1f644e94aa20d4818b5153e1ca7
Thomas Volk <thomas.volk@gmail.com>
2012-02-27 22:46:34 +0100

    run felix task

--- src/main/groovy/de/thomasvolk/gradle/felix/RunFelixTask.groovy
+++ src/main/groovy/de/thomasvolk/gradle/felix/RunFelixTask.groovy
@@ -6,9 +6,6 @@ class RunFelixTask extends BaseFelixTask {
 
     @TaskAction
     def run() {
-        logfile = "$targetDir/felix.log"
-        ant.echo("start felix ...")
-        ant.echo("write output to: $logfile")
-        ant.java(jar: felixMainJar, fork: true, dir: targetDir, output: logfile)
+        ant.java(jar: felixMainJar, fork: true, dir: targetDir)
     }
 }
